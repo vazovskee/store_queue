3167c38d2b7c73ae4e3116b126f009e6ee486376
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-15 18:40:07 +0300

    initial commit: contract template created

---
+++ StoreQueue.sol
@@ -0,0 +1,11 @@
+
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract StoreQueue {
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
+}
