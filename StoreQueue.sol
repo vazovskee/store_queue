5fbe59485c8a9fef149557730afef3bf7c80e5bd
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-15 20:58:20 +0300

    function for dequeuing first person added

--- StoreQueue.sol
+++ StoreQueue.sol
@@ -5,6 +5,7 @@ pragma AbiHeader expire;
 contract StoreQueue {
 
     string[] public queue;
+    uint private firstPosition;
 
     constructor() public {
         require(tvm.pubkey() != 0, 101);
@@ -20,5 +21,11 @@ contract StoreQueue {
 
 	function addPerson(string name) public checkOwnerAndAccept {
         queue.push(name);
+	}
+
+    function excludeFirstPesron() public checkOwnerAndAccept {
+        require(firstPosition < queue.length, 201, "queue is empty");
+        delete queue[firstPosition];
+        firstPosition++;
 	}
 }
