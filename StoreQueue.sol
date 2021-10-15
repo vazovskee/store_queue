613ee457fa7e80f3bbadf996d19d806e1f0e0cd4
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-15 18:46:36 +0300

    function for joining new person in queue added

--- StoreQueue.sol
+++ StoreQueue.sol
@@ -3,9 +3,22 @@ pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 
 contract StoreQueue {
+
+    string[] public queue;
+
     constructor() public {
         require(tvm.pubkey() != 0, 101);
         require(msg.pubkey() == tvm.pubkey(), 102);
         tvm.accept();
     }
+
+    modifier checkOwnerAndAccept {
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+		_;
+	}
+
+	function addPerson(string name) public checkOwnerAndAccept {
+        queue.push(name);
+	}
 }
