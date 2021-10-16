548bb610acaae6dfc4a96fc52fa4292b31796388
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-16 17:32:51 +0300

    some comments were added

--- StoreQueue.sol
+++ StoreQueue.sol
@@ -4,8 +4,8 @@ pragma AbiHeader expire;
 
 contract StoreQueue {
 
-    string[] private queue;
-    uint private firstPosition;
+    string[] private queue;      // массив для хранения имён людей в очереди
+    uint private firstPosition;  // переменная для хранения позиции первого человека в очереди
 
     constructor() public {
         require(tvm.pubkey() != 0, 101);
@@ -19,12 +19,14 @@ contract StoreQueue {
 		_;
 	}
 
+    // функция для добавления имени человека в конец массива
 	function addPerson(string name) public checkOwnerAndAccept {
         queue.push(name);
 	}
 
+    // функция, которая удаляет имя первого человека из очереди и возвращает его значение
     function getPersonOut() public checkOwnerAndAccept returns (string) {
-        require(firstPosition < queue.length, 201, "queue is empty");
+        require(firstPosition < queue.length, 201, "queue is empty"); // проверка на выход за границы массива
         
         string leavingPerson = queue[firstPosition];
         delete queue[firstPosition];
